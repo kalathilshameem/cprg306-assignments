1561716dd1fd46d35f373394bda1d929573a54fb
Kalathil Shameem <your-email@example.com>
2024-09-25 15:37:55 -0600

    Added week-2 page and updated root page

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,10 @@
+import Link from 'next/link';
+
 export default function Page() {
     return (
         <main>
-            <h1>Shopping List</h1>
+            <h1>CPRG 306: Web Development 2 - Assignments</h1>
+            <Link href="/week-2">Go to Week 2 Page</Link>
         </main>
     );
 }
