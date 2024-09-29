d6b3e2f42baf7dda4c39ee662b484e8b79fa14e5
Kalathil Shameem <your-email@example.com>
2024-09-28 19:03:12 -0600

    Add week 3 shopping list feature

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,13 +1,12 @@
-// app/week-2/student-info.js
-import Link from 'next/link'; // Import Link from Next.js
+import Link from "next/link"; // Import Link from Next.js
 
 export default function StudentInfo() {
-    return (
-        <div>
-            <p>Your Name: Muhammed Shameem Kalathil</p>
-            <Link href="https://github.com/kalathilshameem/cprg306-assignments">
-                My GitHub Repository
-            </Link>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Your Name: Muhammed Shameem Kalathil</h2>
+      <Link href="https://github.com/kalathilshameem/cprg306-assignments">
+        My GitHub Repository {/* Link text directly inside the Link component */}
+      </Link>
+    </div>
+  );
 }
